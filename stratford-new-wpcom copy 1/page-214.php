e9e2979a50e6730e2906b280ed061039910c99e8
yenmangu <robert.shelford@googlemail.com>
2022-07-20 23:50:49 +0100

    hopefully removed the max-width

--- stratford-new-wpcom copy 1/page-214.php
+++ stratford-new-wpcom copy 1/page-214.php
@@ -1,32 +1,33 @@
 <head>
-	<style>
-		.deviceName {
-			display: flex;
-			flex-direction: column;
-			padding-left: 2rem;
-			padding-top: 50px;
-			padding-bottom: 100px;
-			gap: 20px;
-		}
+  <style>
+  .deviceName {
+    display: flex;
+    flex-direction: column;
+    padding-left: 2rem;
+    padding-top: 50px;
+    padding-bottom: 100px;
+    gap: 20px;
+  }
 
-		.repairOption {
-			gap: 50px;
-			flex-direction: row-reverse;
+  .repairOption {
+    gap: 50px;
+    flex-direction: row-reverse;
 
-		}
+  }
 
-		.button {
-			margin-top: 20px;
-			border-radius: 6px;
-			width: 250px;
-			/* min-width: 50%; */
-			text-align: center;
-			background-color: rgba(1, 1, 1, 0);
-		}
-		.style-svg {
-			margin-right: 2rem;
-		}
-	</style>
+  .button {
+    margin-top: 20px;
+    border-radius: 6px;
+    width: 250px;
+    /* min-width: 50%; */
+    text-align: center;
+    background-color: rgba(1, 1, 1, 0);
+  }
+
+  .style-svg {
+    margin-right: 2rem;
+  }
+  </style>
 </head>
 
 <?php
@@ -46,9 +47,9 @@ get_header();
 
 
 <section id="primary" class="content-area">
- 	<main id="main" class="site-main">
+  <main id="main" class="site-main">
 
-		<?php
+    <?php
 
 			
 
@@ -66,7 +67,7 @@ get_header();
 			endwhile; // End of the loop.
 		?>
 
-		<?php
+    <?php
 		
 
 			if (isset($_GET['id'])) 
@@ -110,40 +111,43 @@ get_header();
 			// 	$batteryUrl = "http://18.168.90.222/home/brand-new-clinic/gadget-repair/battery-replacement?id=$device[deviceId]";
 
 		?>
-	
-		<div class="deviceName" ">
-			<h4 class="deviceHeading">
-				Services and Repairs for <br>
-				
-			</h4>
-			<h3 class ="deviceSelected"><?=$devices['deviceName']?></h3>
-			<div class="repairOption choose-repair">
-				<p>
-					Choose Repair Type
-		</br> </br>
-				</p>
-				<div class="screenRepair repairOption">
-					<img class="style-svg" src="/wp-content/uploads/2022/07/icons8-broken-phone-64.png" alt="broken-phone-image">
 
-					<a href="http://18.168.90.222/home/brand-new-clinic/gadget-repair/screen-replacement?id=<?=$devices['deviceId']?>" class="button">
-						Screen Replacement
-					</a>
-				</div>
-				<div class="waterDamage repairOption">
-				<img class="style-svg" src="/wp-content/uploads/2022/07/icons8-water-64.png" alt="water-droplet">
-					<a href="http://18.168.90.222/home/brand-new-clinic/gadget-repair/water-damage/?id=<?=$devices['deviceid']?>" class="button">
-						Water Damage
-					</a>
-				</div>
-				<div class="batteryReplacement repairOption">
-				<img class="style-svg" src="/wp-content/uploads/2022/07/icons8-empty-battery-64.png" alt="sad-battery">
-					<a href="http://18.168.90.222/home/brand-new-clinic/gadget-repair/battery-replacement?id=<?=$devices['deviceId']?>" class="button">
-						Battery Replacement
-					</a>
-				</div>
-			</div>
-		</div>
-		
+    <div class="deviceName" ">
+			<h4 class=" deviceHeading">
+      Services and Repairs for <br>
+
+      </h4>
+      <h3 class="deviceSelected"><?=$devices['deviceName']?></h3>
+      <div class="repairOption choose-repair">
+        <p>
+          Choose Repair Type
+          </br> </br>
+        </p>
+        <div class="screenRepair repairOption">
+          <img class="style-svg" src="/wp-content/uploads/2022/07/icons8-broken-phone-64.png" alt="broken-phone-image">
+
+          <a href="http://18.168.90.222/home/brand-new-clinic/gadget-repair/screen-replacement?id=<?=$devices['deviceId']?>"
+            class="button">
+            Screen Replacement
+          </a>
+        </div>
+        <div class="waterDamage repairOption">
+          <img class="style-svg" src="/wp-content/uploads/2022/07/icons8-water-64.png" alt="water-droplet">
+          <a href="http://18.168.90.222/home/brand-new-clinic/gadget-repair/water-damage/?id=<?=$devices['deviceId']?>"
+            class="button">
+            Water Damage
+          </a>
+        </div>
+        <div class="batteryReplacement repairOption">
+          <img class="style-svg" src="/wp-content/uploads/2022/07/icons8-empty-battery-64.png" alt="sad-battery">
+          <a href="http://18.168.90.222/home/brand-new-clinic/gadget-repair/battery-replacement/?id=<?=$devices['deviceId']?>"
+            class="button">
+            Battery Replacement
+          </a>
+        </div>
+      </div>
+    </div>
+
 
   </main><!-- #main -->
 </section><!-- #primary -->
